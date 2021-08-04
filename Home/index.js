f044d6d6096ca5d91972123a75b87c4929cdd398
AnwinShaji <88303594+AnwinShaji@users.noreply.github.com>
2021-08-04 09:59:27 +0530

    Add files via upload

--- Home/index.js
+++ Home/index.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+<?php get_header(); ?>
 
-import App from './App';
+<?php if ( antreas_show_posts() ) : ?>
+	<div id="main" class="main section">
+		<div class="container">
+			<?php antreas_section_heading( 'blog' ); ?>
+			<section id="content" class="content">
+				<?php do_action( 'antreas_before_content' ); ?>
+				<?php antreas_grid( null, 'element', 'blog', 3, array( 'class' => 'column-narrow' ) ); ?>
+				<?php antreas_numbered_pagination(); ?>
+				<?php do_action( 'antreas_after_content' ); ?>
+			</section>
+			<?php get_sidebar(); ?>
+			<div class="clear"></div>
+		</div>
+	</div>
+<?php endif; ?>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+<?php get_footer(); ?>
